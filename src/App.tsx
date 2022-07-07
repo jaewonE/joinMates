fa0dad46c8eab03222a99b2426bff9db2dd5a6ff
jaewonE <importjaewonE@gmail.com>
2022-07-07 15:27:33 +0900

    v1.0.0 project setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,11 @@
 import React from 'react';
+
 function App() {
-  return <div className="App">Hello world</div>;
+  return (
+    <div className="flex justify-center items-center h-screen bg-gray-800">
+      <h1 className="text-7xl text-white font-bold">Hello World!</h1>
+    </div>
+  );
 }
 
 export default App;
